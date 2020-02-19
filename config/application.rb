ae312a3ca75331da4ce20b1d94d20df24495a845
Kuniaki Igarashi <igaiga@gmail.com>
2020-02-19 11:38:35 +0900

    add eager_load setting

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Rails42111Ruby265BooksApp
   class Application < Rails::Application
     config.autoload_paths << "#{config.root}/lib"
+#    config.eager_load_paths << "#{config.root}/lib"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
